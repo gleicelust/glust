e6f54863ca0f1117a3a5afe2bbf33f74c4d42bcd
Gleice Lustosa <lustosacg@gmail.com>
2019-10-03 14:51:39 -0300

    Projeto criado

---
+++ src/main/java/com/glust/idrink/IdrinkApplication.java
@@ -0,0 +1,13 @@
+package com.glust.idrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdrinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdrinkApplication.class, args);
+	}
+
+}
